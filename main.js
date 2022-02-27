15019211b4340f3994407e870d8b155ca60d0808
Michaël Laurac <michaellaurac@gmail.com>
2022-02-27 14:01:10 +0100

    Added string card numbers, updated conversion, and updated tests

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
 const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
 const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
 const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
+const valid6 = "4913143594962082491";
+const valid7 = "6011281136657161355";
+const valid8 = "378440888133909";
+const valid9 = "3542831842199945724";
+const valid10 = "5581691848750339";
+
 
 // All invalid credit card numbers
 const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
@@ -20,11 +26,11 @@ const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
 const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
 
 // An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
+const batch = [valid1, valid2, valid3, valid4, valid5, valid6, valid7, valid8, valid9, valid10, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
 
 // Add your functions below:
 function generateCardFromStringToArray(cardString) {
-  return cardString.split('');
+  return cardString.split('').map( char => parseInt(char) );
 }
 
 function generateCardFromArrayToString(cardArray) {
@@ -32,6 +38,9 @@ function generateCardFromArrayToString(cardArray) {
 }
 
 function validateCred(cardArray) {
+  if (typeof cardArray === 'string') {
+    cardArray = generateCardFromStringToArray(cardArray);
+  }
   const length = cardArray.length;
   const reverseArray = cardArray.map((digit, index) => cardArray[(length - 1) - index ]);
   return reverseArray.map( (digit, index) => {
@@ -50,6 +59,9 @@ function findInvalidCards(nestedCardsArray) {
 }
 
 function idCompanyFromCard(cardArray) {
+  if (typeof cardArray === 'string') {
+    cardArray = generateCardFromStringToArray(cardArray);
+  }
   let companyFound = "Company not found";
   const cardString = generateCardFromArrayToString(cardArray);
   const companyTable = {
@@ -75,6 +87,10 @@ function idCompanyFromCard(cardArray) {
   return companyFound;
 }
 
+function displayValidityAndCompany(cardArray) {
+  console.log("The credit card is " + (validateCred(cardArray) ? "valid" : "invalid") + " (" + idCompanyFromCard(cardArray) + ").");
+}
+
 function idInvalidCardCompanies(nestedCardsArray) {
   const invalidCompanies = [];
   nestedCardsArray.forEach( cardArray => {
@@ -92,29 +108,37 @@ console.log(generateCardFromStringToArray('1234567890123456'));
 console.log(generateCardFromArrayToString(generateCardFromStringToArray('1234567890123456')));
 */
 
-/* Test function validateCred()
-console.log(validateCred(valid1)); // shall return true
-console.log(validateCred(valid2)); // shall return true
-console.log(validateCred(valid3)); // shall return true
-console.log(validateCred(valid4)); // shall return true
-console.log(validateCred(valid5)); // shall return true
-console.log(validateCred(invalid1)); // shall return false
-console.log(validateCred(invalid2)); // shall return false
-console.log(validateCred(invalid3)); // shall return false
-console.log(validateCred(invalid4)); // shall return false
-console.log(validateCred(invalid5)); // shall return false
-console.log(validateCred(mystery1));
-console.log(validateCred(mystery2));
-console.log(validateCred(mystery3));
-console.log(validateCred(mystery4));
-console.log(validateCred(mystery5));
-*/
+/* Test function validateCred() */
+displayValidityAndCompany(valid1);
+displayValidityAndCompany(valid2);
+displayValidityAndCompany(valid3);
+displayValidityAndCompany(valid4);
+displayValidityAndCompany(valid5);
+displayValidityAndCompany(valid6);
+displayValidityAndCompany(valid7);
+displayValidityAndCompany(valid8);
+displayValidityAndCompany(valid9);
+displayValidityAndCompany(valid10);
+
+displayValidityAndCompany(invalid1); // shall return false
+displayValidityAndCompany(invalid2); // shall return false
+displayValidityAndCompany(invalid3); // shall return false
+displayValidityAndCompany(invalid4); // shall return false
+displayValidityAndCompany(invalid5); // shall return false
+
+displayValidityAndCompany(mystery1);
+displayValidityAndCompany(mystery2);
+displayValidityAndCompany(mystery3);
+displayValidityAndCompany(mystery4);
+displayValidityAndCompany(mystery5);
+
 
 /* Test function findInvalidCards()
 console.log(findInvalidCards(batch));
 */
 
-/* Test function idInvalidCardCompanies() */
+/* Test function idInvalidCardCompanies()
 const testNested = findInvalidCards(batch);
 testNested.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]);
 console.log(idInvalidCardCompanies(testNested));
+*/
